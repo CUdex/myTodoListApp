67fd0851c315ec251e053fcfd775d0aa23d2bbae
System Administrator <root@lyuchan-ul-ui-MacBookPro.local>
2022-05-07 16:43:49 +0900

    init app

---
+++ My_TodoListApp/My_TodoListApp/ToDoMainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My_TodoListApp
+//
+//  Created by cudex on 2022/05/07.
+//
+
+import UIKit
+
+class ToDoMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
