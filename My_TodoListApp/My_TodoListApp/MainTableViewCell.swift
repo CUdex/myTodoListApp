a43accc7ccdea0179f66a018b9269bcc4cc6bb1b
System Administrator <root@lyuchan-ul-ui-MacBookPro.local>
2022-05-12 22:58:01 +0900

    기본적인 custom cell 설정 및 cell 간격 설정

--- My_TodoListApp/My_TodoListApp/MainTableViewCell.swift
+++ My_TodoListApp/My_TodoListApp/MainTableViewCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class MainTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var listCellTitleLable: UILabel!
+    @IBOutlet weak var listCellDate: UILabel!
+    @IBOutlet weak var listCellImageView: UIImageView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -18,6 +23,13 @@ class MainTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    // custom cell의 layout 부분을 정의
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        //self.contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16))
     }
     
 }
--- My_TodoListApp/My_TodoListApp/ViewControllers/ToDoMainViewController.swift
+++ My_TodoListApp/My_TodoListApp/ViewControllers/ToDoMainViewController.swift
@@ -11,7 +11,9 @@ class ToDoMainViewController: UIViewController {
 
     @IBOutlet weak var toDoListTable: UITableView!
     
-    var testDate: ToDoCellData?
+    var testDate: [ToDoCellData] = [ToDoCellData(priority: 1, title: "test", date: Date(), description: nil, custom: nil, index: nil)]
+    
+    let spacingSection = 10.0
     
     
     override func viewDidLoad() {
@@ -19,8 +21,63 @@ class ToDoMainViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         self.navigationItem.title = "To Do List"
+        toDoListTable.dataSource = self
+        toDoListTable.delegate = self
+        toDoListTable.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "MainTableViewCell")
+        toDoListTable.sectionHeaderTopPadding = 0
+        
+
     }
 
+}
 
+extension ToDoMainViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    
+    //셀 간격을 위하여 section 활용
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    //각 cell 간격을 위한 section number 설정
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return CGFloat(spacingSection / 2)
+    }
+    // 간격을 위한 view 설정
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        nil
+    }
+    // 간격을 위한 view 설정
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        nil
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return CGFloat(spacingSection / 2)
+    }
+    
+    //cell data
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as! MainTableViewCell
+        
+        let doData = testDate[0]
+        
+        cell.listCellTitleLable.text = doData.title
+        cell.listCellDate.text = doData.date.description
+        
+        return cell
+    }
+    
+    //cell height
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+
+    
 }
 
