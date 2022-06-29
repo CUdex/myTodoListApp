84d7c13bfdf6925065d1c052b15aba0652bb3719
System Administrator <root@lyuchan-ul-ui-MacBookPro.local>
2022-06-29 22:20:47 +0900

    calendar 밑에 들어갈 collectionview cell 구성

---
+++ My_TodoListApp/My_TodoListApp/CalendarCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CalendarCollectionViewCell.swift
+//  My_TodoListApp
+//
+//  Created by cudex on 2022/06/29.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var dateLable: UILabel!
+    @IBOutlet weak var allDayLable: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        print("CalendarCollectionViewCell - awakeFromNib")
+    }
+    
+    
+    
+    
+    
+}
