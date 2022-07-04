2fd1eea2f98076d778983a19220a664da335fd89
System Administrator <root@lyuchan-ul-ui-MacBookPro.local>
2022-07-04 23:17:06 +0900

    detail view 생성

---
+++ My_TodoListApp/My_TodoListApp/CustomViews/detailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  detailViewController.swift
+//  My_TodoListApp
+//
+//  Created by cudex on 2022/07/04.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+
+    @IBOutlet weak var datailView: UIView!
+    @IBOutlet weak var optionView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
